67b3054db9cfbe8e85f24277badd9fccf49bb9e7
mickeykyawzin <mickeykyawzin.cuckoo@gmail.com>
2025-12-05 15:20:05 +0630

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,32 @@
+<!-- navbar -->
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="container-fluid">
+ 
+        <div class="navbar-header">
+            <!-- to enable navigation dropdown when viewed in mobile device -->
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+            
+            <!-- Change "Your Site" to your site name -->
+            <a class="navbar-brand" href="<?php echo "index.php" ?>">DSA Library Management System</a>
+            <a class="navbar-brand" href="read_books.php">စာအုပ်များစာရင်း</a>
+            <a class="navbar-brand" href="search.php">စာအုပ်ရှာရန်</a>
+	    <a class="navbar-brand" href="search_hec.php">HEC e-Catalogue</a>
+		<a class="navbar-brand" href="elib/index.php">e-Books</a>
+		<a class="navbar-brand" href="http://10.1.0.5/">စစ်တက္ကသိုလ်စာကြည့်တိုက်</a>
+		<a class="navbar-brand" href="http://www.myawady.net.mm/">မြဝတီ</a>
+		<a class="navbar-brand" href="http://www.pct.com.mm/">ပြည်ချစ်သား</a>
+		
+        </div>
+ 
+
+             
+        </div><!--/.nav-collapse -->
+ 
+    </div>
+</div>
+<!-- /navbar -->
