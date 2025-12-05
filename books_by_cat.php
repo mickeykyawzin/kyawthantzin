9756f0ad1512766c72d203552891a7829c81dc97
mickeykyawzin <mickeykyawzin.cuckoo@gmail.com>
2025-12-05 09:46:22 +0630

    Add files via upload

---
+++ books_by_cat.php
@@ -0,0 +1,94 @@
+<?php
+// core.php holds pagination variables
+include_once 'config/core.php';
+// include database and object files
+include_once 'config/database.php';
+include_once 'objects/book.php';
+include_once 'objects/category.php';
+
+// instantiate database and objects
+$database = new Database();
+$db = $database->getConnection();
+
+$book = new Book($db);
+$category = new Category($db);
+
+   $category=$_GET['cat'];
+   //if($category=="Ref")
+  // {
+   //  $category="Ref";
+  // }
+   //$category='"'.$category.'"';
+// query products
+   $stmt = $book->byCategories($category, $from_record_num, $records_per_page);
+   $num = $stmt->rowCount();
+  // function countAll_byCategories($category);
+// set page header
+$page_title = "All Books List from category number:".$category;
+include_once "layout_header.php";
+if($num>0){
+    
+    echo "<table class='table table-hover table-responsive table-bordered'>";
+    echo "<tr>";
+    echo "<th>Accession No</th>";
+    echo "<th>Shelf No</th>";
+    echo "<th>Call No</th>";
+    echo "<th>Author</th>";
+    echo "<th>Title</th>";
+    echo "<th>Publisher</th>";
+    echo "<th>Released year</th>";
+    echo "<th>Status</th>";
+    echo "<th>Booking</th>";
+    //echo "<th>remark</th>";
+    echo "</tr>";
+    
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        
+        extract($row);
+        
+        echo "<tr>";
+        echo "<td>{$acc_no}</td>";
+        echo "<td>{$shelf_no}</td>";
+        echo "<td>{$call_no}</td>";
+        echo "<td>{$author}</td>";
+        echo "<td><a href=\"#\" data-toggle=\"popover\" title=\"Book Description\" data-content=\"{$description}\">{$title}</a></td>";
+        echo "<td>{$publisher}</td>";
+        echo "<td>{$released_year}</td>";
+        if($status==1)
+        {
+            
+            echo "<td style=\"color:green\";>Available</td>";
+        }
+        else
+        {
+            echo "<td style=\"color:red\";>Not available</td>";
+            
+        }
+        echo "<td><a href=\"booking.php?call_no=".$call_no."&title=".$title."\">Book</a></td>";
+        //echo "<td>{$remark}</td>";        
+        echo "</tr>";
+        
+    }
+    
+    echo "</table>";
+    // the page where this paging is used
+    $page_url = "books_by_cat.php?cat=".$category."&";
+    
+    // count all products in the database to calculate total pages
+    $total_rows = $book->countAll_byCategories($category);
+    
+    // read_template.php controls how the product list will be rendered
+    //include_once "read_template.php";
+    
+    // paging buttons here
+    include_once 'paging.php';
+}
+
+// tell the user there are no products
+else{
+    echo "<div class='alert alert-info'>No products found.</div>";
+}
+
+// set page footer
+include_once "layout_footer.php";
+?>
