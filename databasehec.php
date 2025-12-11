d2727b474c8eac607959556671cc2f96d3d89392
mickeykyawzin <mickeykyawzin.cuckoo@gmail.com>
2025-12-11 15:18:51 +0630

    Add files via upload

---
+++ databasehec.php
@@ -0,0 +1,25 @@
+<?php
+class DatabaseHec{
+  
+    // specify your own database credentials
+    private $host = "localhost";
+    private $db_name = "heclib";
+    private $username = "admin";
+    private $password = "password";
+    public $conn;
+  
+    // get the database connection
+    public function getConnection(){
+  
+        $this->conn = null;
+  
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+  
+        return $this->conn;
+    }
+}
+?>
